384119d380a3af09f04fb2bf6778c7acd4538108
masamoe <105935014+masamoe@users.noreply.github.com>
2023-01-23 16:47:59 -0800

    rearrange

---
+++ ClientServer202301/ClientServer202301/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Program p = new Program();
+
+            string str = "my string";
+            var str2 = str.ToUpper();
+            
+            
+        }
+    }
+}
---
+++ ClntSrvr2023-01/ClntSrvr2023-01/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+            program.TestHostInformation();
+        }
+
+        private void TestHostInformation()
+        {
+            var hostInformation = new Network.HostInformation();
+            var hostEntry = hostInformation.GetHostInformation("localhost");
+            Console.WriteLine(hostEntry.HostName);
+        }
+    }
+}
