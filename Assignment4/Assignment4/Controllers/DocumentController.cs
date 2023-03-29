a588143c3ac5185d74e66e3b75eb0fbde96da092
masamoe <105935014+masamoe@users.noreply.github.com>
2023-03-29 13:13:31 -0700

    ff

---
+++ Assignment4/Assignment4/Controllers/DocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment4.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
