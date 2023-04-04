9130dc54d23077836cf547b415f887656685710c
masamoe <105935014+masamoe@users.noreply.github.com>
2023-04-03 18:45:41 -0700

    apr3

---
+++ Assignment4/DataModels/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels
+{
+    public class Document
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Index { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Assignment4/Interfaces/IDocumentRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataModels;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface IDocumentRepository
+    {
+        Document Get(string Id);
+
+        List<Document> GetAll(string title);
+
+        void Add(Document document);
+
+        void Remove(string id);
+
+        void Update(string id, Document document);
+    }
+}
